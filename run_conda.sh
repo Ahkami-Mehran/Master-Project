5aaf40276c9deec3387c2474320b147eb2756a98
Mehran Ahkami <ahkami.mehran@gmail.com>
2022-01-16 16:56:23 +0100

    create script files

---
+++ run_conda.sh
@@ -0,0 +1,16 @@
+#PBS -N convmixer_1024_20_corrupted
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=1:gpus=1,mem=1gb,walltime=24:00:00
+#PBS -m ae
+#PBS -q student
+
+model='convmixer_1024_20'
+classes=1000
+checkpoint="./trained_models/convmixer_1024_20_ks9_p14.pth.tar"
+output="results.json"
+data="./data/dev" 
+
+echo `hostname`
+eval "$(conda shell.bash hook)"
+conda activate mp
+python validate.py --model $model --b 64 --num-classes $classes --checkpoint $checkpoint --results-file $output $data
