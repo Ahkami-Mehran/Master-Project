da08db961fbe672507688f238234f70cea0b581d
Mehran Ahkami <ahkami.mehran@gmail.com>
2022-01-17 11:24:37 +0100

    new scripts to try

--- run_venv.sh
+++ run_venv.sh
@@ -1,14 +1,2 @@
-#PBS -N convmixer_1024_20_corrupted
-#PBS -S /bin/bash
-#PBS -l nodes=1:ppn=1:gpus=1,mem=1gb,walltime=24:00:00
-#PBS -m ae
-#PBS -q student
-
-model='convmixer_1024_20'
-classes=1000
-checkpoint="./trained_models/convmixer_1024_20_ks9_p14.pth.tar"
-output="results.json"
-data="/misc/lmbraid19/datasets/ImageNet/Corrupted" 
-
 source "/misc/student/ahkamian/Master-Project/.venv/bin/activate"
-python validate.py --model $model --b 64 --num-classes $classes --checkpoint $checkpoint --results-file $output $data
+python validate.py --model $1 --b 64 --num-classes $2 --checkpoint $3 --results-file $4 $5
---
+++ script.sh
@@ -0,0 +1,13 @@
+#PBS -N convmixer_1024_20_corrupted
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=1:gpus=1,mem=1gb,walltime=24:00:00
+#PBS -m ae
+#PBS -q student
+model='convmixer_1024_20'
+classes=1000
+checkpoint="./trained_models/convmixer_1024_20_ks9_p14.pth.tar"
+output="results.json"
+data="/misc/lmbraid19/datasets/ImageNet/Corrupted"
+
+chmod +x /misc/student/ahkamian/Master-Project/run_venv.sh
+/misc/student/ahkamian/Master-Project/run_venv.sh $model $classes $checkpoint $output $data
